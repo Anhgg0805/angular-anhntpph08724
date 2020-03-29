d94b2abac025d23f7a4720644fe41b0d79b1a2f2
Anhgg0805 <anhsowon@gmail.com>
2020-03-29 14:10:40 +0700

    3 files modified

--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -30,13 +30,13 @@ export class ProductManagerComponent implements OnInit {
     // this.products=this.productService.removeProduct(id);
     this.productService.removeProduct(id).subscribe(response =>{
       this.router.navigate(['/admin/product-manager'])
-      // console.log(response);
+      console.log(response);
     });
   }
   addProduct(){
     // this.productService.addProduct(this.productNew);
     this.productService.addProduct(this.productNew).subscribe(data => {
-    this.router.navigate(['//product-manager'])
+    this.router.navigate(['/admin/product-manager'])
     });
     
   }
